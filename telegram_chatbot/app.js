357faccd8b471da7bab94fbfff54ba8aa4180206
Potatomochi <noahrsam@gmail.com>
2022-12-26 00:14:52 +0700

    Bot form creation

--- telegram_chatbot/app.js
+++ telegram_chatbot/app.js
@@ -1,5 +1,6 @@
 
 const dotenv=require('dotenv')
+// require('./booking.js')
 dotenv.config({path:'../.env'});
 
 const { CHATBOT_TOKEN, SERVER_URL } = process.env
@@ -9,16 +10,63 @@ const token = CHATBOT_TOKEN
 const bot = new TelegramBot(token, {polling: true})
 module.exports = bot
 
-bot.on('message', (msg) => {
-    bot.sendMessage(msg.chat.id,"Welcome to Kommune's Chatbot! Please select an option or type 'Booking' to get started. If you need help, do /help ",{
-        "reply_markup": {
-            "keyboard": [["Booking"],   ["Enquiries/Help"], ["Cancel"]]
-            }
-        });
-    if(msg.text.toString().toLowerCase().includes("booking")){
-        return
-    }
+// bot.on('message', (msg) => {
+//     bot.sendMessage(msg.chat.id,"Welcome to Kommune's Chatbot! Please select an option or type 'Booking' to get started. If you need help, do /help ",{
+//         "reply_markup": {
+//             "keyboard": [["Booking"],   ["Enquiries/Help"], ["Cancel"]]
+//             }
+//         });
+//     if(msg.text.toString().toLowerCase().includes("booking")){
+//         return
+//     }
     
+// });
+// bot.onText(/\/idiot/, (msg) => {
+
+//     bot.sendMessage(msg.chat.id, "this is to check");
+    
+//     });
+var answerCallbacks = {};
+bot.on('message', function (message) {
+    var callback = answerCallbacks[message.chat.id];
+    if (callback) {
+        delete answerCallbacks[message.chat.id];
+        return callback(message);
+    }
 });
+
+
+bot.onText(/\/booking/, function (message) {
+     bot.sendMessage(message.chat.id, "Hello! Thanks for your interest in Kommune, if you would like to make a booking, could we have your mobile number?").then(function () {
+         answerCallbacks[message.chat.id] = function (answer) {
+             var mobileNumber = answer.text;
+             bot.sendMessage(message.chat.id, "Please select an activity you would like to book (Selective options) (Selective options)" , {
+                "reply_markup": {
+                    "keyboard": [["Karaoke"],   ["stuff"], ["Idinahoi"]]
+                    }
+                }).then(function () {
+                 answerCallbacks[message.chat.id] = function (answer) {
+                     var option = answer.text;
+                     bot.sendMessage(message.chat.id, "How many people are you bringing to Kommune (including yourself) ?").then(function () {
+                         answerCallbacks[message.chat.id] = function (answer) {
+                             var people = answer.text;
+                             bot.sendMessage(message.chat.id, "Can we have the date for booking").then(function (){
+                                answerCallbacks[message.chat.id] = function (answer) {
+                                    var date = answer.text;
+                                    bot.sendMessage(message.chat.id, "What is the duration of your booking in Kommune?").then(function(){
+                                        answerCallbacks[message.chat.id] = function (answer){
+                                            var duration = answer.text
+                                        }
+                                    })
+                                }
+                             });
+                         }
+                     });
+                 }
+             });
+         }
+     });
+     bot.sendMessage(message.chat.id,"check me")
+ });
 
 console.log('working')
--- telegram_chatbot/booking.js
+++ telegram_chatbot/booking.js
@@ -1,7 +1,8 @@
-const { keyboard } = require('telegraf/typings/markup');
 const bot = require('./app');
 
-bot.onText('booking', function (message) {
+var answerCallbacks = {};
+
+bot.onText(/\/booking/, function (message) {
      bot.sendMessage(message.chat.id, "Hello! Thanks for your interest in Kommune, if you would like to make a booking, could we have your mobile number?").then(function () {
          answerCallbacks[message.chat.id] = function (answer) {
              var mobileNumber = answer.text;
@@ -15,11 +16,21 @@ bot.onText('booking', function (message) {
                      bot.sendMessage(message.chat.id, "How many people are you bringing to Kommune (including yourself) ?").then(function () {
                          answerCallbacks[message.chat.id] = function (answer) {
                              var people = answer.text;
-                             bot.sendMessage(message.chat.id, name + address + phone + " saved!").then(function ());
+                             bot.sendMessage(message.chat.id, "Can we have the date for booking").then(function (){
+                                answerCallbacks[message.chat.id] = function (answer) {
+                                    var date = answer.text;
+                                    bot.sendMessage(message.chat.id, "What is the duration of your booking in Kommune?").then(function(){
+                                        answerCallbacks[message.chat.id] = function (answer){
+                                            var duration = answer.text
+                                        }
+                                    })
+                                }
+                             });
                          }
                      });
                  }
              });
          }
      });
+     bot.sendMessage(message.chat.id,"check me")
  });
